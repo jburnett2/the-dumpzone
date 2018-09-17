e9bc15db4bd7167d4b669c96da9d9a7f7fd62b39
jburnett2 <jburnett@trinity.edu>
2018-09-17 17:08:28 -0500

    first commit

---
+++ Assets/Scripts/touchCat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchCat : MonoBehaviour {
+
+    public Sprite untouch;
+    public Sprite touch;
+
+    private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = untouch;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    
+    }
+
+    private void OnMouseDown()
+    {
+        sr.sprite = touch;
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.25f, transform.position.z);
+    }
+
+    private void OnMouseUp()
+    {
+        sr.sprite = untouch;
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.25f, transform.position.z);
+    }
+
+}
